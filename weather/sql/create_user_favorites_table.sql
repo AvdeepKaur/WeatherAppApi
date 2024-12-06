ccca19fe3e35b0f3469513efe2d565c0ddce0b2c
AvdeepKaur <avkaur@bu.edu>
2024-12-06 13:02:46 -0500

    condensed database

--- weather/sql/create_user_favorites_table.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS user_favorites;
-CREATE TABLE user_favorites (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER NOT NULL,
-    location_name TEXT NOT NULL,
-    latitude REAL NOT NULL,
-    longitude REAL NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id)
-);
--- weather/sql/create_user_table.sql
+++ weather/sql/create_user_table.sql
@@ -5,3 +5,13 @@ CREATE TABLE users (
     email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL
 );
+
+DROP TABLE IF EXISTS user_favorites;
+CREATE TABLE user_favorites (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    location_name TEXT NOT NULL,
+    latitude REAL NOT NULL,
+    longitude REAL NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
